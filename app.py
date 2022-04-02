c461e32f4b729f802b033cd0b8d13ae0d6d61e0b
Mwathi <mwathibrian7@gmail.com>
2022-04-02 09:19:35 +0300

    Model predict 3-5-7-30 days

--- app.py
+++ app.py
@@ -1,70 +1,20 @@
 from flask import Flask, jsonify
-import numpy as np
-import pickle
-from tensorflow import keras
 import weather
 
-
-scaler = pickle.load(open('scaler.sav', 'rb'))
-model = keras.models.load_model('rainfall-model')
 app = Flask(__name__)
-look_back_steps = 60
-no_of_features = 4
-
-
-# Convert X value to shape required for MinMax scaler i.e (28,4)
-def format_model_input(x_input):
-    # convert input into numpy array
-    data = np.array(x_input)
-    data = data.reshape((look_back_steps, no_of_features))
-    last_col = np.zeros((look_back_steps, 1))
-    return np.append(data, last_col, axis=1)
-
-# Scale the x_input
-
-
-def scale_transform(df):
-    return scaler.fit_transform(df)
-
-# Invert the scaled y_pred
-
-
-def rescale_y_pred(y_pred):
-    y_pred = y_pred.ravel()  # convert from 2D array into 1D array
-    # create a new numpy array
-    shape = (y_pred.shape[0], no_of_features + 1)
-    n_array = np.zeros(shape)
-    n_array[:, -1] = y_pred
-    return n_array
-
-
-def reshape_scaled_x_for_prediction(scaled_df):
-    X = scaled_df[:, :-1]
-    x_shape = (1, look_back_steps, no_of_features)
-    return X.reshape(x_shape)
 
 
 @app.route('/forecast', methods=['GET'])
 def rainfall_forecast():
-    weather_ndarray = weather.get_ndarray_weather(
-        lookback_days=look_back_steps)
-    scaled_data = scale_transform(weather_ndarray)
-    X_scaled = reshape_scaled_x_for_prediction(scaled_df=scaled_data)
-    y_pred = model.predict(X_scaled)
-    inv_yhat = scaler.inverse_transform(rescale_y_pred(y_pred))[:, -1]
-    rainfall_forecast = inv_yhat.tolist()
-    human_readable_forecast = weather.build_human_readable_forecast(
-        rainfall_forecast)
-    print(len(human_readable_forecast))
+    human_readable_forecast = weather.forecast_rainfall()
     return jsonify(human_readable_forecast)
 
 
 @app.route('/past-weather', methods=['GET'])
 def get_past_weather():
-    weather_data = weather.get_ndarray_weather(
-        lookback_days=look_back_steps).tolist()
-    print(len(weather_data))
-    return jsonify(weather_data)
+    weather_data_json = weather.get_weather_json(
+        lookback_days=weather.look_back_steps)
+    return weather_data_json
 
 
 if __name__ == '__main__':
--- weather.py
+++ weather.py
@@ -1,13 +1,46 @@
-import datetime
-from urllib.request import urlopen
 import json
+import pickle
+import datetime
 import numpy as np
+from tensorflow import keras
+from urllib.request import urlopen
+
+import weather
+
+scaler = pickle.load(open('scaler.sav', 'rb'))
+model = keras.models.load_model('rainfall-model-30-day')
+look_back_steps = 60
+no_of_features = 4
 
 latitude = -1.5177
 longitude = 37.2634
 end_date = datetime.date.today() - datetime.timedelta(3)
 end_date_str = (end_date).strftime("%Y%m%d")
 weather_params = 'T2M,PS,WS10M,QV2M,PRECTOTCORR'
+
+
+# Scale the x_input
+
+
+def scale_transform(df):
+    return scaler.fit_transform(df)
+
+# Invert the scaled y_pred
+
+
+def rescale_y_pred(y_pred):
+    y_pred = y_pred.ravel()  # convert from 2D array into 1D array
+    # create a new numpy array
+    shape = (y_pred.shape[0], no_of_features + 1)
+    n_array = np.zeros(shape)
+    n_array[:, -1] = y_pred
+    return n_array
+
+
+def reshape_scaled_x_for_prediction(scaled_df):
+    X = scaled_df[:, :-1]
+    x_shape = (1, look_back_steps, no_of_features)
+    return X.reshape(x_shape)
 
 
 def get_weather_json(lookback_days):
@@ -48,4 +81,18 @@ def build_human_readable_forecast(forecasts):
             "forecast": forecasts[index]
         })
 
+    return human_readable_forecast
+
+
+def forecast_rainfall():
+    weather_ndarray = get_ndarray_weather(
+        lookback_days=look_back_steps)
+    scaled_data = scale_transform(weather_ndarray)
+    X_scaled = reshape_scaled_x_for_prediction(scaled_df=scaled_data)
+    y_pred = model.predict(X_scaled)
+    inv_yhat = scaler.inverse_transform(rescale_y_pred(y_pred))[:, -1]
+    rainfall_forecast = inv_yhat.tolist()
+    human_readable_forecast = weather.build_human_readable_forecast(
+        rainfall_forecast)
+    print(len(human_readable_forecast))
     return human_readable_forecast
